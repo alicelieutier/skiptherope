5fece39e1cc39a84bd48a153bde5a681570e9f56
Alice Lieutier <alice.lieutier@gmail.com>
2013-02-23 12:20:56 +0000

    testing the accelerometer

--- accelerometer.js
+++ accelerometer.js
@@ -1,12 +1,18 @@
 function start () {
-	api.watch();
-	var r = ;
-
-	var li = document.createElement('li');
-	li.textContent = r;
-	document.getElementById('results').appendChild(li);
+	window.addEventListener("devicemotion", logAcceleration, true);
 }
 
 function stop() {
+	window.removeEventListener("devicemotion", logAcceleration);
+}
 
+function logAcceleration(event) {
+  var row = document.createElement('tr');
+	row.innerHTML = "<tr>
+		<td> " + Date.now() + " </td>
+		<td> " + event.acceleration.toString() + " </td>
+		<td> " + event.accelerationIncludingGravity.toString() + " </td>
+		<td> " + event.interval.toString() + " </td>
+		</tr>"
+	document.getElementById('results').appendChild(row);
 }
